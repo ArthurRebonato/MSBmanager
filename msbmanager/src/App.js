165fd6a3266bac5fc6706bd9544992a9cb41c44e
Arthur <arthur.rebonato@hotmail.com>
2021-11-18 17:13:55 -0300

    Ajuste Services e variáveis Firebase

--- msbmanager/src/App.js
+++ msbmanager/src/App.js
@@ -1,8 +1,94 @@
+import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import LoginIcon from '@mui/icons-material/Login';
+import AppRegistrationIcon from '@mui/icons-material/AppRegistration';
+import Stack from '@mui/material/Stack';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import React, {useState, useLayoutEffect} from 'react';
+import {storageSave, storageRemove, storageGet} from "./services/Storage"
 
 function App() {
+  const [lembreme, setLembreme] = useState(false)
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  
+  useLayoutEffect(() => {
+      let emailStorage = storageGet("email")
+      let passwordStorage = storageGet("password")
+
+      if (emailStorage){
+        setEmail(emailStorage)
+        setPassword(passwordStorage)
+        setLembreme(true)
+      }
+    }, [])
+
+    const handleLembreme = (e) => {
+      setLembreme(e.target.checked)
+ 
+      if(e.target.checked === true){
+       storageSave("email", email)
+       storageSave("password", password)
+      } else {
+       storageRemove("email")
+       storageRemove("password")
+      }
+    }
+
   return (
-    <>
-    </>
+    <Grid container spacing={1}>
+        <Grid item xs={3}></Grid>
+        <Grid item xs={6}>
+          <TextField type="email" 
+          id="filled-basic" 
+          label="E-mail" 
+          variant="filled" 
+          fullWidth 
+          margin="dense"
+          value={email}
+          onChange={(e)=> setEmail(e.target.value)}
+          />
+        </Grid>
+        <Grid item xs={3}></Grid>
+        <Grid item xs={3}></Grid>
+        <Grid item xs={6}>
+          <TextField type="password" 
+          id="filled-basic" 
+          label="Senha" 
+          variant="filled" 
+          fullWidth 
+          margin="dense"
+          value={password}
+          onChange={(e)=> setPassword(e.target.value)}
+          />
+        </Grid>
+        <Grid item xs={3}></Grid>
+        <Grid item xs={3}></Grid>
+        <Grid item xs={3}>
+          <FormGroup>
+            <FormControlLabel control={<Checkbox checked={lembreme} 
+            onChange={handleLembreme} />} 
+            label="Lembre-me" />
+          </FormGroup>
+          <Stack direction="row" spacing={2}>
+            <Button variant="outlined" 
+            size="medium" 
+            startIcon={<AppRegistrationIcon/> }
+            fullWidth>
+              Registrar
+            </Button>
+            <Button variant="contained" 
+            size="medium" 
+            endIcon={<LoginIcon />} 
+            fullWidth>
+              Logar
+            </Button>
+          </Stack>
+        </Grid>
+      </Grid>
   );
 }
 
---
+++ msbmanager/src/services/Firebase.js
@@ -0,0 +1,13 @@
+import {initializeApp} from "firebase/app"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCiOEGf-E4y84R8fywoaGUZVD6FVjPRVVk",
+  authDomain: "msbmanager-30389.firebaseapp.com",
+  projectId: "msbmanager-30389",
+  storageBucket: "msbmanager-30389.appspot.com",
+  messagingSenderId: "459603832870",
+  appId: "1:459603832870:web:6395b9480db2f9029476e7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
